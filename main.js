c0c737a0477fc701181ec876d0e961321775217a
Domenik228 <akbarbrawlstarsnick@gmail.com>
2025-03-07 16:28:37 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+let ism = prompt("ismingizni kiriting");
+
+let id = prompt("space IDingizni kiriting");
+
+let password = prompt("space passwordingizni kiriting");
+
+let object = {
+    name: ism,
+    id: id,
+    password: password
+}
+
+console.log(object);
+
